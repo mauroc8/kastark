9de235c2108f65c1dc6fae5ad11d48e5515b8bfd
Mauro Cano <maurocanobrusa@gmail.com>
2019-09-25 00:12:50 -0300

    Volver al viejo Shield en 2D

---
+++ Assets/Scripts/Habilities/Shield/ShieldController2D.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Events;
+using UnityEngine.EventSystems;
+
+public class ShieldController2D : HabilityController
+{
+
+    [Header("Refs")]
+    [SerializeField] RectTransform   _circleTransform = null;
+
+    [Header("Animation")]
+    [SerializeField] float _speed = 1;
+
+    [Header("Cast")]
+    [SerializeField] float _maxCastDistanceVh = 0.1f;
+
+    float _effectiveness;
+    bool _hovering;
+
+    public float Effectiveness => _effectiveness;
+    public bool Hovering => _hovering;
+    public bool Cast => _cast;
+
+    Vector2 _unitScreenPos;
+    float _maxCastDistancePx;
+
+    void Start() {
+        _maxCastDistancePx = Screen.height * _maxCastDistanceVh;
+    }
+
+    void Update() {
+        if (_cast) return;
+
+        _unitScreenPos = Camera.main.WorldToScreenPoint(GameState.actingCreature.chest.position);
+        
+        _effectiveness = GetEffectiveness(Time.time);
+        
+        _effectiveness = Mathf.Pow(_effectiveness, difficulty);
+
+        var scale         = 0.5f + 0.5f * _effectiveness;
+
+        _circleTransform.localScale = new Vector3(scale, scale, 1);
+
+        _hovering = Vector2.Distance(Input.mousePosition, _unitScreenPos) < _maxCastDistancePx;
+
+        if (Input.GetMouseButtonDown(0) && _hovering && !Util.MouseIsOnUI())
+        {
+            _cast = true;
+            var habilityCastController = new HabilityCastController{
+                targets = new CreatureController[]{ GameState.actingCreature },
+                effectiveness = new float[]{ _effectiveness },
+                hability = GameState.selectedHability
+            };
+            habilityCastController.Cast();
+        }
+    }
+
+    float GetEffectiveness(float t)
+    {
+        var cycle = (Time.time * _speed) % 2;
+        if (cycle > 1) cycle = 2 - cycle;
+
+        return cycle;
+    }
+}
---
+++ Assets/Scripts/Habilities/Shield/ShieldHabilityUIController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldHabilityUIController : MonoBehaviour
+{
+    [SerializeField] ShieldController2D _shieldController = null;
+
+    [Header("Collor controllers")]
+    [SerializeField] ColorController _backgroundColor = null;
+    [SerializeField] ColorController     _circleColor = null;
+
+    [Header("Settings")]
+    [SerializeField] float _changeCircleColorThreshold = 0.9f;
+    [SerializeField] float _colorBlendThreshold = 0.08f;
+
+    [Header("Colors")]
+    [SerializeField] Color _defaultCircleColor = Color.white;
+    [SerializeField] Color _alternativeCircleColor = Color.yellow;
+
+    [Header("Cursor")]
+    [SerializeField] CursorSkin _cursorSkin = null;
+
+    bool _lastHovering;
+
+    void Update()
+    {
+        // Color.
+        _backgroundColor.ChangeAlpha(_shieldController.Effectiveness);
+
+        if (_shieldController.Effectiveness >= _changeCircleColorThreshold)
+            _circleColor.ChangeColor(_alternativeCircleColor);
+        else
+        {
+            var diff = _changeCircleColorThreshold - _shieldController.Effectiveness;
+            Color color;
+            if (diff <= _colorBlendThreshold)
+                color = Color.Lerp(_alternativeCircleColor, _defaultCircleColor, diff / _colorBlendThreshold);
+            else
+                _circleColor.ChangeColor(_defaultCircleColor);
+        }
+        
+        _circleColor.ChangeAlpha(_shieldController.Effectiveness);
+
+        // Cursor.
+        if (_shieldController.Hovering)
+        {
+            if (!_lastHovering)
+            {
+                _cursorSkin.ChangeCursorTexture(CursorTexture.Friendly);
+            }
+        }
+        else
+        {
+            if (_lastHovering)
+            {
+                _cursorSkin.ChangeCursorTexture(CursorTexture.None);
+            }
+        }
+
+        _lastHovering = _shieldController.Hovering;
+    }
+}
