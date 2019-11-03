0d50ecd6e2628c1c2b1fb75ad322a29a57450bb9
Mauro Cano <maurocanobrusa@gmail.com>
2019-11-02 23:48:21 -0300

    Restaurando Menúes. Los borré sin querer cuando moví cosas de lugar.

---
+++ Assets/Menu Scenes/1-Choose Language/ChangeLanguageSettings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using StringLocalization;
+using UnityEngine;
+
+public class ChangeLanguageSettings : MonoBehaviour
+{
+    [SerializeField] LanguageSettings _gameSettings = null;
+
+    public void ChangeLanguageToSpanish()
+    {
+        _gameSettings.ChangeLanguage(Language.Spanish);
+        _gameSettings.Save();
+    }
+
+    public void ChangeLanguageToEnglish()
+    {
+        _gameSettings.ChangeLanguage(Language.English);
+        _gameSettings.Save();
+    }
+}
---
+++ Assets/Menu Scenes/2-Main Menu/MainMenuButtonHandlers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonHandlers : MonoBehaviour
+{
+    [SerializeField] MoveToNextSceneController _sceneController = null;
+    
+    public void NewGame()
+    {
+        _sceneController.MoveToNextScene();
+    }
+
+    public void ChangeLanguage()
+    {
+        _sceneController.MoveToPreviousScene();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
