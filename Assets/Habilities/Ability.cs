a7a9b563627008b9466fc79eb6d5db4c7ead1f79
Mauro Cano <maurocanobrusa@gmail.com>
2020-08-19 18:02:10 -0300

    Build of page

--- Assets/Habilities/Ability.cs
+++
@@ -1,8 +0,0 @@
-using System.Threading;
-using System.Threading.Tasks;
-using UnityEngine;
-
-public abstract class Ability : UpdateAsStream
-{
-    public abstract bool IsAgressive { get; }
-}
--- Assets/Scripts/CameraToolComponent.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-[RequireComponent(typeof(CameraController))]
-public class CameraToolComponent : MonoBehaviour
-{
-}
